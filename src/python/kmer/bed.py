d2d7419f84a29d08fdd8494a3a83525e8114ffef
parsoa <parsoakhorsand91@gmail.com>
2017-09-29 16:59:52 -0700

    Implement extraction of kmers from inverted sequence

--- src/python/kmer/bed.py
+++ src/python/kmer/bed.py
@@ -3,6 +3,7 @@ import sys
 from kmer import (
     reference,
     config,
+    sets,
 )
 
 import pybedtools
@@ -11,13 +12,19 @@ import khmer
 def read_tracks_from_bed_file(path):
     c = config.Configuration()
     counts = khmer.Counttable(c.ksize, c.khmer_table_size, c.khmer_num_tables)
+    inverse_counts = khmer.Counttable(c.ksize, c.khmer_table_size, c.khmer_num_tables)
     bedtools = pybedtools.BedTool(path)
     for track in bedtools:
         sequence = extract_reference_sequence(track)
-        head, tail = extract_sequence_boundaries(sequence)
-        print(counts.get_kmers(head))
+        (head, tail), (inverse_head, inverse_tail) = extract_sequence_boundaries(sequence)
+        # 
         counts.consume(head)
         counts.consume(tail)
+        # 
+        inverse_counts.consume(inverse_head)
+        inverse_counts.consume(inverse_tail)
+        # 
+    print('common:', sets.calc_set_intersection())
 
 def extract_reference_sequence(track):
     # TODO: this might actually cause problem if it surpasses the boundaris of the chromosome
@@ -29,17 +36,25 @@ def extract_reference_sequence(track):
     return sequence.seqfn
 
 def extract_sequence_boundaries(sequence):
-    print(sequence)
+    # print(sequence)
     c = config.Configuration()
     f = open(sequence)
     for i, line in enumerate(f):
         line = line.strip()
         if i == 1:
+            # print('#', line, '#')
             head = line[0:2 * c.ksize]
             tail = line[-2 * c.ksize:]
-            print('head: ', head)
-            print('tail: ', tail)
-            return head, tail
+            # print('head: ', head)
+            # print('tail: ', tail)
+            # inverse this sequence
+            line = line[::-1]
+            # print('#', line, '#')
+            inverse_head = line[0:2 * c.ksize]
+            inverse_tail = line[-2 * c.ksize:]
+            # print('inverse head: ', inverse_head)
+            # print('inverse tail: ', inverse_tail)
+            return (head, tail), (inverse_head, inverse_tail)
 
 def extract_sample_sequence(track):
     bedtool = pybedtools.BedTool(str(track), from_string=True)
