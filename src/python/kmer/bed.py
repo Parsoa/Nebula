0a6590c0ba56475b548ae85ff2e0e3c907810410
parsoa <parsoakhorsand91@gmail.com>
2017-10-03 15:54:11 -0700

    Temporarily disable FASTQ couting

--- src/python/kmer/bed.py
+++ src/python/kmer/bed.py
@@ -34,11 +34,11 @@ def read_tracks_from_bed_file(path):
             for kmer in counts.get_kmers(seq) :
                 inverse_kmers[kmer.upper()] = True
     print('done processing tracks')
-    fastq_counts = khmer.Counttable(c.ksize, c.khmer_table_size, c.khmer_num_tables)
-    total_reads, n_consumed = fastq_counts.consume_seqfile(c.fastq_file)
-    print('fatsq, total_reads: ', total_reads, ', n_consumed: ', n_consumed)
-    print('reference kmers:', kmers)
-    print('inverse reference kmers:', inverse_kmers)
+    # fastq_counts = khmer.Counttable(c.ksize, c.khmer_table_size, c.khmer_num_tables)
+    # total_reads, n_consumed = fastq_counts.consume_seqfile(c.fastq_file)
+    # print('fatsq, total_reads: ', total_reads, ', n_consumed: ', n_consumed)
+    print('reference kmers:', len(kmers))
+    print('inverse reference kmers:', len(inverse_kmers))
     print('common:', sets.print_dictionary_keys(
         sets.calc_dictionary_intersection(kmers, inverse_kmers)))
 
