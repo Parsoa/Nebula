1af935888142ecb63632f23d9d9e10070d617d3d
Parsoa <parsoakhorsand91@gmail.com>
2017-10-18 15:50:58 -0700

    Fix BrokenPipeError in test.py

--- src/python/kmer/test.py
+++ src/python/kmer/test.py
@@ -3,9 +3,9 @@ import socket
 
 if __name__ == '__main__':
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(('localhost', 6985))
     while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(('localhost', 6985))
         time.sleep(2)
         len_sent = s.send(b'hello kire khar')
         response = s.recv(len_sent)
