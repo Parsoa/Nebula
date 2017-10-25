dccf76f62c73bf03f97b5b464b98b330e3c412bd
parsoa <parsoakhorsand91@gmail.com>
2017-10-25 12:01:03 -0700

    Update .gitignore

---
+++ output/merge_sv.py
@@ -0,0 +1,11 @@
+import io
+import json
+
+tracks = {}
+for i in range(0, 12):
+    with open('./batch_' + str(i) + '.json') as json_file:
+        batch = json.load(json_file)
+        tracks.update(batch)
+with open('./inversion_boundaries.json') as json_file:
+    json.dump(tracks, json_file)
+
