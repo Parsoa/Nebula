64d8e9220343751a2890e81a4a12f7ac3109ccf7
parsoa <parsoakhorsand91@gmail.com>
2017-10-25 12:05:32 -0700

    Fix file access mode bug in merge_sv.py

--- output/merge_sv.py
+++ output/merge_sv.py
@@ -6,6 +6,6 @@ for i in range(0, 12):
     with open('./batch_' + str(i) + '.json') as json_file:
         batch = json.load(json_file)
         tracks.update(batch)
-with open('./inversion_boundaries.json') as json_file:
+with open('./inversion_boundaries.json', 'w') as json_file:
     json.dump(tracks, json_file)
 
