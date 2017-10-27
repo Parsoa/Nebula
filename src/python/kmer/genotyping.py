ea123fff5536ae07124ad94c5e8853eb3967c336
Parsoa <parsoakhorsand91@gmail.com>
2017-10-27 01:49:12 -0700

    Implement basic boundary breadth-first search

--- src/python/kmer/genotyping.py
+++ src/python/kmer/genotyping.py
@@ -25,6 +25,18 @@ import colorama
 import pybedtools
 
 # ============================================================================================================================ #
+# ============================================================================================================================ #
+# ============================================================================================================================ #
+
+class Genotype(object):
+
+    def __init__(self, head, tail):
+        self.head = head
+        self.tail = tail
+        self.kmers = bed.count_boundary_kmers(head, tail)
+        # print('distinc kmers in genotype: ', len(self.kmers))
+
+# ============================================================================================================================ #
 # Execution
 # ============================================================================================================================ #
 
@@ -45,6 +57,7 @@ def refine_variation_boundaries():
         batch[index].append(track)
         print('assigned ', name, ' to ', index)
         n = n + 1
+        break
     # run each batch in a separate process
     children = []
     for index in batch:
@@ -75,23 +88,57 @@ def run_batch(tracks, index):
 
 @commons.measure_time
 def find_track_boundaries(sv , index):
-    max = None
+    c = config.Configuration()
     frontier = {}
     for begin in range(-radius, radius + 1) :
         for end in range(-radius, radius + 1) :
-            try :
-                interval = (begin, end)
-                print(interval)
-                print(colorama.Fore.GREEN + '--------------------------------------------------------')
-                head, tail = sv.get_interval_boundaries(begin, end)
-                score, boundary = calc_boundary_score(head, tail, index)
-                # print(colorama.Fore.GREEN + 'score: ', score)
-                max = (score, boundary, interval) if not max else\
-                    (score, boundary, interval) if score > max[0] else max
-            except Exception as e:
-                print(e)
-                traceback.print_exc()
-                print(colorama.Fore.RED + 'boundary error, skipping')
+            head, tail = sv.get_interval_boundaries(begin, end)
+            genotype = Genotype(head = head, tail = tail)
+            key = str(begin) + '_' + str(end)
+            frontier[key] = genotype
+    # there will be 2 *c.ksize kmers at max
+    for i in range(0, 2 * c.ksize) :
+        print('i = ', i)
+        remove = {}
+        for genotype in frontier :
+            # print(genotype)
+            genotype = frontier[genotype]
+            keys = list(genotype.kmers.keys())
+            kmers = []
+            n = 0
+            if 2 * i < len(keys):
+                kmers.append(keys[2 * i])
+                n = 1
+                if (2 * i) + 1 < len(keys):
+                    kmers.append(keys[2 * i + 1])
+                    n = 2
+            else:
+                # due to repeats, it is possible that less than 2*ksize unique kmers appear
+                continue
+            score = calc_similarity_score(kmers, index)
+            if score != n :
+                remove[genotype] = True
+        for genotype in remove:
+            frontier.pop(genotype, None)
+    # check whatever that is left in the frontier
+    print(frontier)
+    # now check the reference counts to find the best match
+    max = None
+    # for begin in range(-radius, radius + 1) :
+    #     for end in range(-radius, radius + 1) :
+    #         try :
+    #             interval = (begin, end)
+    #             print(interval)
+    #             print(colorama.Fore.GREEN + '--------------------------------------------------------')
+    #             head, tail = sv.get_interval_boundaries(begin, end)
+    #             score, boundary = calc_boundary_score(head, tail, index)
+    #             # print(colorama.Fore.GREEN + 'score: ', score)
+    #             max = (score, boundary, interval) if not max else\
+    #                 (score, boundary, interval) if score > max[0] else max
+    #         except Exception as e:
+    #             print(e)
+    #             traceback.print_exc()
+    #             print(colorama.Fore.RED + 'boundary error, skipping')
     # print(colorama.Fore.GREEN + '########################################################')
     print('choice: ', max)
     return max
