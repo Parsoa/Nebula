fc70ec95687567a463a3a765ac90495703e6a7e5
Parsoa <parsoakhorsand91@gmail.com>
2017-10-30 15:51:30 -0700

    Modify count_sevrer kill script to use pkill instead of parsing ps output

--- src/python/kill_count_server.sh
+++ src/python/kill_count_server.sh
@@ -1,5 +1,6 @@
 ps -A | grep "python -m kmer.sv"
-for pid in $(ps -A | grep "python -m kmer.count_server" | cut -c 1-5); do
-    kill -9 $pid
-done
+# for pid in $(ps -A | grep "python -m kmer.count_server" | cut -c 1-5); do
+#     kill -9 $pid
+# done
+pkill -f "count_server"
 ps -A | grep "python -m kmer.sv"
