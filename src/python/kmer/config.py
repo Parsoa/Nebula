00ca6b020729990bf2750245a4dac870f667c7b1
Parsoa <parsoakhorsand91@gmail.com>
2017-11-03 05:22:45 -0700

    Add default value for config bed_file option

--- src/python/kmer/config.py
+++ src/python/kmer/config.py
@@ -59,7 +59,7 @@ class Configuration:
 # Configuration
 # ============================================================================================================================ #
 
-def configure(reference_genome = 'hg38', bed_file = CHM1_Lumpy.Del.100bp.bed):
+def configure(reference_genome = 'hg38', bed_file = "CHM1_Lumpy.Del.100bp.bed"):
     if sys.platform == "darwin":
         print('Running on Mac OS X')
         genome_chm1 = os.path.abspath(os.path.join(os.path.dirname(__file__),\
